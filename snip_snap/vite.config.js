432ea3daca3a4346f2fbe91cb9b6888f3358d2eb
farhathaydar7 <haydarfarhatsteam69@gmail.com>
2025-03-21 11:44:16 +0200

    refixed the bug completly

--- snip_snap/vite.config.js
+++ snip_snap/vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: {
-      ".js": "jsx", // Enable JSX syntax in .js files
-    },
-  },
+  plugins: [
+    react({
+      // This tells Vite to apply JSX transformations to .js files too
+      include: /\.(jsx|js)$/,
+    }),
+  ],
 });
